5a42ff977ce61b5fc97ce1ea5007071e9ebb6dd8
richtr <rich.tibbett@gmail.com>
2013-09-17 19:17:10 +1000

    Fix a couple of issues with visual gesture controls

--- js/gestures.js
+++ js/gestures.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
   var transitionPropNames = ['transition', 'OTransition', 'MSTransition', 'MozTransition', 'WebkitTransition'];
   var transformPropNames = ['transform', 'OTransform', 'MSTransform', 'MozTransform', 'WebkitTransform'];
       
-  function getCSSPropName(propNameSearchArray, defaultPropName) {
+  function getCSSPropertyName(propNameSearchArray, defaultPropName) {
     var el = document.createElement('div');
     var style = el.style;
     for (var i = 0, l = propNameSearchArray.length; i < l; i++) {
@@ -21,12 +21,12 @@ document.addEventListener("DOMContentLoaded", function() {
     return defaultPropName; // fallback to standard prop name
   }
 
-  var transitionCSSPropName = getCSSPropName(transitionPropNames, 'transition');
-  var transformCSSPropName = getCSSPropName(transformPropNames, 'transform');
+  var transitionCSSPropName = getCSSPropertyName(transitionPropNames, 'transition');
+  var transformCSSPropName = getCSSPropertyName(transformPropNames, 'transform');
 
   var startX, startY;
   var hChange = 0, vChange = 0;
-  var navInvoked = false;
+  var navState = 0; // pending state
   
   // Disable object dragging
   swipePane.addEventListener('dragstart', function(evt) {
@@ -36,93 +36,108 @@ document.addEventListener("DOMContentLoaded", function() {
   // Prevent element clicking in desktop user agents if 
   // navigation actions have been invoked
   swipePane.addEventListener('click', function(evt) {
-    if(navInvoked) {
-      navInvoked = false;
+    if(navState == 3) { // invoked state
+      navState = 0; // pending state
       evt.preventDefault();
     }
   });
   
+  function startHandler(evt) {
+    if(evt.type == 'touchstart') {
+      startX = evt.touches[0].pageX;
+      startY = evt.touches[0].pageY;
+    } else { // mousedown
+      startX = evt.clientX;
+      startY = evt.clientY;
+    }
+    
+    navState = 1; // started state
+    
+    // Start page navigation drag tracking
+    swipePane.addEventListener(evt.type == 'touchstart' ? 'touchmove' : 'mousemove', moveHandler);
+  }
+  
   function moveHandler(evt) {
-    hChange = evt.changedTouches[0].pageX - startX;
-    vChange = evt.changedTouches[0].pageY - startY;
-
+    var moveX, moveY;
+    
+    if(evt.type == 'touchmove') {
+      moveX = evt.changedTouches[0].pageX;
+      moveY = evt.changedTouches[0].pageY;
+    } else { // mousemove
+      moveX = evt.clientX;
+      moveY = evt.clientY;
+    }
+    
+    hChange = moveX - startX;
+    vChange = moveY - startY;
+    
     // Disable horizontal scroll but enable vertical scrolling
     if(Math.abs(hChange)>10 && Math.abs(hChange) > Math.abs(vChange)) {
       // Only prevent default behavior if a previous or next link is available
-      if ((hChange > 0 && previousLink) || (hChange < 0 && nextLink)) {
+      if((hChange > 0 && previousLink) || (hChange < 0 && nextLink)) {
         evt.preventDefault();
       }
+      
+      // Animate prev/next overlay elements
+      if (previousLink && hChange > 10) {
+        if(hChange < 150) {
+          previousOverlay.style[transformCSSPropName] = 'translate3d(' + hChange + 'px,0,0)';
+          previousOverlay.style.opacity = hChange / 150;
+          previousOverlay.style.backgroundColor = '#3c3c3c';
+        } else {
+          previousOverlay.style.opacity = 0.9;
+          previousOverlay.style[transformCSSPropName] = 'translate3d(150px,0,0)';
+          previousOverlay.style.backgroundColor = '#000';
+        }
+      } else if(nextLink && hChange < -10) {
+        if(hChange > -150) {
+          nextOverlay.style[transformCSSPropName] = 'translate3d(' + hChange + 'px,0,0)';
+          nextOverlay.style.opacity = hChange / -150;
+          nextOverlay.style.backgroundColor = '#3c3c3c';
+        } else {
+          nextOverlay.style.opacity = 0.9;
+          nextOverlay.style[transformCSSPropName] = 'translate3d(-150px,0,0)';
+          nextOverlay.style.backgroundColor = '#000';
+        }
+      }
     }
     
-    // Animate prev/next overlay elements
-    if (previousLink && hChange > 20) {
-      if(hChange < 150) {
-        previousOverlay.style[transformCSSPropName] = 'translate3d(' + hChange + 'px,0,0)';
-        previousOverlay.style.opacity = hChange / 150;
-        previousOverlay.style.backgroundColor = '#3c3c3c';
-      } else {
-        previousOverlay.style.opacity = 0.9;
-        previousOverlay.style[transformCSSPropName] = 'translate3d(150px,0,0)';
-        previousOverlay.style.backgroundColor = '#000';
-      }
-    } else if (nextLink && hChange < 20) {
-      if(hChange > -150) {
-        nextOverlay.style[transformCSSPropName] = 'translate3d(' + hChange + 'px,0,0)';
-        nextOverlay.style.opacity = hChange / -150;
-        nextOverlay.style.backgroundColor = '#3c3c3c';
-      } else {
-        nextOverlay.style.opacity = 0.9;
-        nextOverlay.style[transformCSSPropName] = 'translate3d(-150px,0,0)';
-        nextOverlay.style.backgroundColor = '#000';
-      }
+    if(navState == 1) { // started state
+      swipePane.addEventListener(evt.type == 'touchmove' ? 'touchend' : 'mouseup', endHandler);
+      navState = 2; // running state
     }
   }
-
-  swipePane.addEventListener('touchstart', function(evt) {
-    startX = evt.touches[0].pageX;
-    startY = evt.touches[0].pageY;
-    
-    // Start page navigation drag tracking
-    swipePane.addEventListener('touchmove', moveHandler);
-  });
   
-  swipePane.addEventListener('touchend', function(evt) {
+  function endHandler(evt) {
     // Stop page navigation drag tracking
-    swipePane.removeEventListener('touchmove', moveHandler);
+    swipePane.removeEventListener(evt.type == 'touchend' ? 'touchmove' : 'mousemove', moveHandler);
     
-    if(hChange > 10) {
-      navInvoked = true;
-      if(hChange >= 150) {
-        if(previousLink) {
-          previousOverlay.style.width = '100%';
-          window.location.href = previousLink.href;
-        }
-      } else {
-        previousOverlay.style[transitionCSSPropName] = "all 0.3s ease";
-        setTimeout(function() {
-          previousOverlay.style[transitionCSSPropName] = null;
-        }, 350);
-        previousOverlay.style[transformCSSPropName] = 'translate3d(0,0,0)';
-      }
-    } else if (hChange < -10) {
-      navInvoked = true;
-      if(hChange <= -150) {
-        if(nextLink) {
-          nextOverlay.style.width = '100%';
-          window.location.href = nextLink.href;
-        }
-      } else {
-        nextOverlay.style[transitionCSSPropName] = "all 0.3s ease";
-        setTimeout(function() {
-          nextOverlay.style[transitionCSSPropName] = null;
-        }, 350);
-        nextOverlay.style[transformCSSPropName] = 'translate3d(0,0,0)';
+    if(Math.abs(hChange) >= 150) {
+      navState = 3; // invoked state
+      if(previousLink && hChange >= 150) {
+        previousOverlay.style.width = '100%';
+        window.location.href = previousLink.href;
+        return;
+      } else if(nextLink && hChange <= -150) {
+        nextOverlay.style.width = '100%';
+        window.location.href = nextLink.href;
+        return;
       }
     } else {
-      navInvoked = false;
+      navState = 0; // pending state
+      
+      previousOverlay.style[transitionCSSPropName] = nextOverlay.style[transitionCSSPropName] = "all 0.3s ease";
+      setTimeout(function() {
+        previousOverlay.style[transitionCSSPropName] = nextOverlay.style[transitionCSSPropName] = null;
+      }, 350);
+      previousOverlay.style[transformCSSPropName] = nextOverlay.style[transformCSSPropName] = 'translate3d(0,0,0)';
+      
     }
     
-    hChange = vChange = 0;
-  });
+    swipePane.removeEventListener('touchend', endHandler);
+  }
+
+  swipePane.addEventListener('touchstart', startHandler);
+  swipePane.addEventListener('mousedown', startHandler);
 
 });
